b191fe7b67c73f4a65d2eb45312c53a9bf16e8cd
Anik Paul Gomes <gomesap29@gmail.com>
2020-04-15 19:21:34 -0500

    screenshot added

--- SourceCode/SparkDataFrame/SparkDataFrame.py
+++ SourceCode/SparkDataFrame/SparkDataFrame.py
@@ -12,8 +12,8 @@ df = spark.read.csv(r"survey.csv", header=True)
 df.createOrReplaceTempView("Survey")
 
 #writing the data into a file
-#df.show()
-#df.write.option("header", "true").csv("newSurvey.csv")
+df.show()
+df.write.option("header", "true").csv("newSurvey.csv")
 
 #Checking for duplicates and drop it from the data
 df.dropDuplicates()
@@ -30,7 +30,7 @@ spark.sql("select treatment, count(*) as TreatmentCount from Survey group by tre
 spark.sql("select * from TechCompany as tc left join NonTechCompany as ntc on tc.Gender == ntc.Gender ").show()
 spark.sql("select Country, count(*) as Count from Survey group by Country order by Count desc").show()
 
-
+#display 13th row
 row13 = df.limit(13)
 expr = [f.last(col).alias(col) for col in row13.columns]
 row13.agg(*expr).show()
